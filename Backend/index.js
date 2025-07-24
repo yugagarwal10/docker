68b88475882c9359fa200634abcb1817cbabdd58
Yug agarwal <yugagarwal214@gmail.com>
2025-07-24 16:14:33 +0530

    test

---
+++ Backend/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
+});
